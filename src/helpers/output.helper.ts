0221b0eb8dd3b1d8e27073bd907872a29780b731
Luuk Rijnbende <luuk@rijnbende.nl>
2021-07-19 14:53:04 +0200

    feat: use rest param to allow for multiple params to output test

--- src/helpers/output.helper.ts
+++ src/helpers/output.helper.ts
@@ -4,27 +4,28 @@ import { Util } from "../util";
 
 declare module "../piumino" {
     interface Piumino<T> {
-        testOutput(selector: Selector, output: string, destination: string, modifyValue?: any): TestDefinition;
+        testOutput(selector: Selector, output: string, destination: string, ...modifyValue: any): TestDefinition;
     }
 }
 
-Piumino.prototype.testOutput = function(selector, output, destination, modifyValue) {
+Piumino.prototype.testOutput = function(selector, output, destination, ...modifyValue) {
     return [
-        `should wire output "${output}" to ${Util.getSelectorName(selector)}`,
+        `should wire output '${output}' to ${Util.getSelectorName(selector)}`,
         () => {
             const element = Util.getElementBySelector(this.fixture, selector);
-            const isComponent = element.nativeNode instanceof HTMLUnknownElement;
+            const isAngularType = Util.isAngularType(element);
             jest.spyOn(this.component, destination);
 
             expect(this.component[destination]).not.toHaveBeenCalled();
 
-            if (modifyValue) {
-                isComponent
-                    ? element.componentInstance[output].emit(modifyValue)
-                    : element.nativeElement.dispatchEvent(new CustomEvent(output, { detail: modifyValue }));
-                expect(this.component[destination]).toHaveBeenCalledWith(modifyValue);
+            if (modifyValue.length) {
+                const emitValue = modifyValue[modifyValue.length - 1];
+                isAngularType
+                    ? element.componentInstance[output].emit(emitValue)
+                    : element.nativeElement.dispatchEvent(new CustomEvent(output, { detail: emitValue }));
+                expect(this.component[destination]).toHaveBeenCalledWith(...modifyValue);
             } else {
-                isComponent
+                isAngularType
                     ? element.componentInstance[output].emit()
                     : element.nativeElement.dispatchEvent(new Event(output));
                 expect(this.component[destination]).toHaveBeenCalled();
