e38f1119b672fb3d6459ea8f05c9a13784323b17
Luuk Rijnbende <luuk@rijnbende.nl>
2022-06-25 22:10:49 +0200

    wip(demo): start with demo tests

--- demo/jest/src/app/app.component.spec.ts
+++ demo/jest/src/app/app.component.spec.ts
@@ -16,25 +16,6 @@ describe('AppComponent', () => {
     piumino.init(fixture);
   });
 
-  it(...piumino.expect("[data-testid=\"to-have-text\"]").toHaveText("test").build());
-
-
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-
-  // it(`should have as title 'angular-starter'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('angular-starter');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('angular-starter app is running!');
-  // });
+  it(...piumino.expect("[data-testid=\"to-have-text\"]").toHaveText("Should have this text").build());
+  it(...piumino.expect("[data-testid=\"to-have-text\"]").toHaveText("Should fail").build());
 });
